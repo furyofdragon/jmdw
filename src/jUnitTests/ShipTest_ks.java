a0f734d81aa3d96991a3bb3a39843b6974e24667
IT <furyofdragon@gmail.com>
2012-10-03 00:30:30 +0400

    Dependency from org.eclipse.swt project removed.

---
+++ src/jUnitTests/ShipTest_ks.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package jUnitTests;
+
+//import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author IT
+ *
+ */
+public class ShipTest_ks {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		//Ship shiptest = new Ship();
+		//fail(if(shiptest.ks > 4));
+	}
+
+}
