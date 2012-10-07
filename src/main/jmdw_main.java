03b689a946d73a77360eb850a05cc1ef9c9dcd17
furyofdragon <furyofdragon@gmail.com>
2012-10-08 02:19:10 +0400

    Small localization

--- src/main/jmdw_main.java
+++ src/main/jmdw_main.java
@@ -12,6 +12,7 @@ import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.GridData;
+import messages.Messages;
 
 public class jmdw_main {
 	private static Text HeightOfWaves;
@@ -30,123 +31,116 @@ public class jmdw_main {
 	 */
 	public static void main(String[] args) {
 		
-		//Text constants
-		final String appName						= "Java MDW";
-		final String grpShipsClassText				= "Ship's class";
-		final String grpShipsTypeText				= "Ship's type";
-		final String grpShipssCharacteristicsText	= "Ships's characteristics";
-		final String grpResultsText					= "Results";
-		
 		final int hWindow = 400;
 		final int wWindow = 600;
 		Display display = Display.getDefault();
 		Shell shlJavaMdw = new Shell();
 		shlJavaMdw.setMinimumSize(new Point(wWindow, hWindow));
 		shlJavaMdw.setSize(wWindow, hWindow);
-		shlJavaMdw.setText(appName);
+		shlJavaMdw.setText(Messages.getString("jmdw_main.shlJavaMdw.text")); //$NON-NLS-1$
 		shlJavaMdw.setLayout(new GridLayout(4, true));
 
 		Group grpShipsClass = new Group(shlJavaMdw, SWT.NONE);
 		grpShipsClass.setLayout(new GridLayout(2, true));
 		grpShipsClass.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		grpShipsClass.setText(grpShipsClassText);
+		grpShipsClass.setText(Messages.getString("jmdw_main.grpShipsClass.text")); //$NON-NLS-1$
 
 		final Button button_M = new Button(grpShipsClass, SWT.RADIO);
 		button_M.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		button_M.setSelection(true);
-		button_M.setText("\u041C");
+		button_M.setText(Messages.getString("jmdw_main.button_M.text")); //$NON-NLS-1$
 		
 				final Button button_MSP = new Button(grpShipsClass, SWT.RADIO);
 				button_MSP.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-				button_MSP.setText("\u041C-\u0421\u041F");
+				button_MSP.setText(Messages.getString("jmdw_main.button_MSP.text")); //$NON-NLS-1$
 
 		final Button button_O = new Button(grpShipsClass, SWT.RADIO);
 		button_O.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		button_O.setText("\u041E");
+		button_O.setText(Messages.getString("jmdw_main.button_O.text")); //$NON-NLS-1$
 		
 				final Button button_MPR = new Button(grpShipsClass, SWT.RADIO);
 				button_MPR.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-				button_MPR.setText("\u041C-\u041F\u0420");
+				button_MPR.setText(Messages.getString("jmdw_main.button_MPR.text")); //$NON-NLS-1$
 
 		final Button button_R = new Button(grpShipsClass, SWT.RADIO);
 		button_R.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		button_R.setText("\u0420");
+		button_R.setText(Messages.getString("jmdw_main.button_R.text")); //$NON-NLS-1$
 		
 				final Button button_OPR = new Button(grpShipsClass, SWT.RADIO);
 				button_OPR.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-				button_OPR.setText("\u041E-\u041F\u0420");
+				button_OPR.setText(Messages.getString("jmdw_main.button_OPR.text")); //$NON-NLS-1$
 
 		final Button button_L = new Button(grpShipsClass, SWT.RADIO);
 		button_L.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		button_L.setText("\u041B");
+		button_L.setText(Messages.getString("jmdw_main.button_L.text")); //$NON-NLS-1$
 		new Label(grpShipsClass, SWT.NONE);
 		
 		final Group grpShipsType = new Group(shlJavaMdw, SWT.NONE);
 		grpShipsType.setLayout(new GridLayout(1, true));
 		grpShipsType.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		grpShipsType.setText(grpShipsTypeText);
+		grpShipsType.setText(Messages.getString("jmdw_main.grpShipsType.text")); //$NON-NLS-1$
 		
 		final Button btnCargoShip = new Button(grpShipsType, SWT.RADIO);
 		btnCargoShip.setSelection(true);
-		btnCargoShip.setText("cargo ship");
+		btnCargoShip.setText(Messages.getString("jmdw_main.btnCargoShip.text")); //$NON-NLS-1$
 		
 		final Button btnPassangerShip = new Button(grpShipsType, SWT.RADIO);
-		btnPassangerShip.setText("passanger ship");
+		btnPassangerShip.setText(Messages.getString("jmdw_main.btnPassangerShip.text")); //$NON-NLS-1$
 		
 		final Button btnTowingShip = new Button(grpShipsType, SWT.RADIO);
-		btnTowingShip.setText("towing ship");
+		btnTowingShip.setText(Messages.getString("jmdw_main.btnTowingShip.text")); //$NON-NLS-1$
 				
 		Group grpShipssCharacteristics = new Group(shlJavaMdw, SWT.NONE);
 		grpShipssCharacteristics.setLayout(new GridLayout(2, true));
 		grpShipssCharacteristics.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
-		grpShipssCharacteristics.setText(grpShipssCharacteristicsText);
+		grpShipssCharacteristics.setText(Messages.getString("jmdw_main.grpShipssCharacteristics.text")); //$NON-NLS-1$
 		
 		Label lblLM = new Label(grpShipssCharacteristics, SWT.NONE);
 		lblLM.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		lblLM.setText("L, m");
+		lblLM.setText(Messages.getString("jmdw_main.lblLM.text")); //$NON-NLS-1$
 		
 		text_L = new Text(grpShipssCharacteristics, SWT.BORDER | SWT.RIGHT);
 		text_L.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		
 		Label lblBM = new Label(grpShipssCharacteristics, SWT.NONE);
 		lblBM.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		lblBM.setText("B, m");
+		lblBM.setText(Messages.getString("jmdw_main.lblBM.text")); //$NON-NLS-1$
 		
 		text_B = new Text(grpShipssCharacteristics, SWT.BORDER | SWT.RIGHT);
 		text_B.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		
 		Label lblTM = new Label(grpShipssCharacteristics, SWT.NONE);
 		lblTM.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		lblTM.setText("T, m");
+		lblTM.setText(Messages.getString("jmdw_main.lblTM.text")); //$NON-NLS-1$
 		
 		text_T = new Text(grpShipssCharacteristics, SWT.BORDER | SWT.RIGHT);
 		text_T.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		
 		Label lblTfM = new Label(grpShipssCharacteristics, SWT.NONE);
 		lblTfM.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		lblTfM.setText("Tf, m");
+		lblTfM.setText(Messages.getString("jmdw_main.lblTfM.text")); //$NON-NLS-1$
 		
 		text_Tf = new Text(grpShipssCharacteristics, SWT.BORDER | SWT.RIGHT);
 		text_Tf.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		
 		Label label = new Label(grpShipssCharacteristics, SWT.NONE);
 		label.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		label.setText("\u03B4");
+		label.setText(Messages.getString("jmdw_main.label.text")); //$NON-NLS-1$
 		
 		text_delta = new Text(grpShipssCharacteristics, SWT.BORDER | SWT.RIGHT);
 		text_delta.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		
 		Label lblHeightOfWaves = new Label(shlJavaMdw, SWT.NONE);
 		lblHeightOfWaves.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, true, true, 1, 1));
-		lblHeightOfWaves.setText("Height of waves, m");
+		lblHeightOfWaves.setText(Messages.getString("jmdw_main.lblHeightOfWaves.text")); //$NON-NLS-1$
 		
 		Label lblShipsSpeedKmh = new Label(shlJavaMdw, SWT.NONE);
 		lblShipsSpeedKmh.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, true, true, 1, 1));
-		lblShipsSpeedKmh.setText("Ship's speed, km/h");
+		lblShipsSpeedKmh.setText(Messages.getString("jmdw_main.lblShipsSpeedKmh.text")); //$NON-NLS-1$
 		
 		Label lblMomentOfInertia = new Label(shlJavaMdw, SWT.NONE);
 		lblMomentOfInertia.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, true, true, 2, 1));
-		lblMomentOfInertia.setText("Moment of inertia\r\nof cross section, m^4");
+		lblMomentOfInertia.setText(Messages.getString("jmdw_main.lblMomentOfInertia.text")); //$NON-NLS-1$
 		
 		text_Speed = new Text(shlJavaMdw, SWT.BORDER | SWT.RIGHT);
 		text_Speed.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
@@ -160,11 +154,11 @@ public class jmdw_main {
 		Group grpResults = new Group(shlJavaMdw, SWT.NONE);
 		grpResults.setLayout(new GridLayout(4, true));
 		grpResults.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 4, 1));
-		grpResults.setText(grpResultsText);
+		grpResults.setText(Messages.getString("jmdw_main.grpResults.text")); //$NON-NLS-1$
 		
 		final Label lblMv = new Label(grpResults, SWT.NONE);
 		lblMv.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		lblMv.setText("Mv");
+		lblMv.setText(Messages.getString("jmdw_main.lblMv.text")); //$NON-NLS-1$
 		
 		final Text lblNewLabel_0 = new Text(grpResults, SWT.BORDER | SWT.RIGHT);
 		lblNewLabel_0.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
@@ -174,7 +168,7 @@ public class jmdw_main {
 		
 		final Label lblKp = new Label(grpResults, SWT.NONE);
 		lblKp.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		lblKp.setText("kp");
+		lblKp.setText(Messages.getString("jmdw_main.lblKp.text")); //$NON-NLS-1$
 		
 		final Text lblNewLabel_1 = new Text(grpResults, SWT.BORDER | SWT.RIGHT);
 		lblNewLabel_1.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
@@ -182,7 +176,7 @@ public class jmdw_main {
 		
 		Label lblMdw = new Label(grpResults, SWT.NONE);
 		lblMdw.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		lblMdw.setText("Mdw");
+		lblMdw.setText(Messages.getString("jmdw_main.lblMdw.text")); //$NON-NLS-1$
 		
 		final Text lblNewLabel_3 = new Text(grpResults, SWT.BORDER | SWT.RIGHT);
 		lblNewLabel_3.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
@@ -190,7 +184,7 @@ public class jmdw_main {
 		
 		final Label lblMy = new Label(grpResults, SWT.NONE);
 		lblMy.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		lblMy.setText("My");
+		lblMy.setText(Messages.getString("jmdw_main.lblMy.text")); //$NON-NLS-1$
 		
 		final Text lblNewLabel_2 = new Text(grpResults, SWT.BORDER | SWT.RIGHT);
 		lblNewLabel_2.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
@@ -200,12 +194,12 @@ public class jmdw_main {
 		
 		Button btnSolve = new Button(shlJavaMdw, SWT.NONE);
 		btnSolve.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
-		btnSolve.setText("Solve");
+		btnSolve.setText(Messages.getString("jmdw_main.btnSolve.text")); //$NON-NLS-1$
 		
 		Button btnSaveResults = new Button(shlJavaMdw, SWT.NONE);
 		btnSaveResults.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
 		btnSaveResults.setEnabled(false);
-		btnSaveResults.setText("Save results");
+		btnSaveResults.setText(Messages.getString("jmdw_main.btnSaveResults.text")); //$NON-NLS-1$
 		
 		
 		SelectionListener btnSolveSelectionListener = new SelectionAdapter(){
