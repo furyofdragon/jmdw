6e5f479fb0add5f5e5bb152368a1c36729059391
IT <furyofdragon@gmail.com>
2012-10-18 01:05:39 +0400

    data in results

--- src/main/PrintResults.java
+++ src/main/PrintResults.java
@@ -17,25 +17,24 @@ public class PrintResults {
 			pw.println("<TR><TD align=\"center\"><B>Input data</B></TD></TR>");
 			pw.println("<TR><TD>" + Messages.getString("jmdw_main.grpShipsClass.text")		+ "</TD><TD></TD></TR>");
 			pw.println("<TR><TD>" + Messages.getString("jmdw_main.grpShipsType.text")		+ "</TD><TD></TD></TR>");
-			pw.println("<TR><TD>" + Messages.getString("jmdw_main.lblLM.text")				+ "</TD><TD></TD></TR>");
-			pw.println("<TR><TD>" + Messages.getString("jmdw_main.lblBM.text")				+ "</TD><TD></TD></TR>");
-			pw.println("<TR><TD>" + Messages.getString("jmdw_main.lblTM.text")				+ "</TD><TD></TD></TR>");
-			pw.println("<TR><TD>" + Messages.getString("jmdw_main.lblTfM.text")				+ "</TD><TD></TD></TR>");
-			pw.println("<TR><TD>" + Messages.getString("jmdw_main.label.text")				+ "</TD><TD></TD></TR>");
-			pw.println("<TR><TD>" + Messages.getString("jmdw_main.lblShipsSpeedKmh.text")	+ "</TD><TD></TD></TR>");
-			pw.println("<TR><TD>" + Messages.getString("jmdw_main.lblMomentOfInertia.text")	+ "</TD><TD></TD></TR>");
-			pw.println("<TR><TD>" + Messages.getString("jmdw_main.lblHeightOfWaves.text")	+ "</TD><TD></TD></TR>");
+			pw.println("<TR><TD>" + Messages.getString("jmdw_main.lblLM.text")				+ "</TD><TD>" + Double.toString(jmdw_main.ship.getL())     + "</TD></TR>");
+			pw.println("<TR><TD>" + Messages.getString("jmdw_main.lblBM.text")				+ "</TD><TD>" + Double.toString(jmdw_main.ship.getB())     + "</TD></TR>");
+			pw.println("<TR><TD>" + Messages.getString("jmdw_main.lblTM.text")				+ "</TD><TD>" + Double.toString(jmdw_main.ship.getT())     + "</TD></TR>");
+			pw.println("<TR><TD>" + Messages.getString("jmdw_main.lblTfM.text")				+ "</TD><TD>" + Double.toString(jmdw_main.ship.getTf())    + "</TD></TR>");
+			pw.println("<TR><TD>" + Messages.getString("jmdw_main.label.text")				+ "</TD><TD>" + Double.toString(jmdw_main.ship.getdelta()) + "</TD></TR>");
+			pw.println("<TR><TD>" + Messages.getString("jmdw_main.lblShipsSpeedKmh.text")	+ "</TD><TD>" + Double.toString(jmdw_main.ship.getSpeed()) + "</TD></TR>");
+			pw.println("<TR><TD>" + Messages.getString("jmdw_main.lblMomentOfInertia.text")	+ "</TD><TD>" + Double.toString(jmdw_main.ship.getI())     + "</TD></TR>");
+			pw.println("<TR><TD>" + Messages.getString("jmdw_main.lblHeightOfWaves.text")	+ "</TD><TD>" + Double.toString(jmdw_main.ship.geth())     + "</TD></TR>");
 			pw.println("<TR><TD align=\"center\"><B>Results</B></TD></TR>");
 			pw.println("<TR><TD>"    + Messages.getString("jmdw_main.lblMv.text")	+ "</TD><TD>"        + Double.toString(jmdw_main.ship.getMv())  + "</TD></TR>");
 			pw.println("<TR><TD>"    + Messages.getString("jmdw_main.lblKp.text")	+ "</TD><TD>"        + Double.toString(jmdw_main.ship.getkp())  + "</TD></TR>");
 			pw.println("<TR><TD>"    + Messages.getString("jmdw_main.lblMy.text")	+ "</TD><TD>"        + Double.toString(jmdw_main.ship.getMy())  + "</TD></TR>");
 			pw.println("<TR><TD><B>" + Messages.getString("jmdw_main.lblMdw.text")	+ "</B></TD><TD><B>" + Double.toString(jmdw_main.ship.getMdw()) + "</B></TD></TR>");
-			
-			
-			
 			pw.println("</TBODY></TABLE>");
 			pw.println("</BODY></HTML>");
+			
 			pw.close();
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
